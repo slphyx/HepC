809e415879de1736dc5a5a1864db5ae0f1b535e8
tanaphum <tana_jumbo@hotmail.com>
2018-10-04 08:24:50 +0700

    update web
    -add download button
    -update ui/css

--- web/server.R
+++ web/server.R
@@ -13,173 +13,219 @@ library(deSolve)
 library(reshape2)
 library(ggplot2)
 library(tidyverse)
+library(xlsx)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  
-  
-  
-  
-  
-  
+
   v <- reactiveValues(doPlot = FALSE)
   options(scipen=6)
-
+  
   observeEvent(input$go, {
     # 0 will be coerced to FALSE
     # 1+ will be coerced to TRUE
     v$doPlot <- input$go
   })
   
-  observeEvent(input$sidebarinput, {
+  observeEvent(input$reset, {
     v$doPlot <- FALSE
   })  
-
+    
     setwd("C:/hepc/web")
     sourceCpp('p1.cpp')
+    parameter <-function(){
+      parms <- list(
+        K= input$K,        #Maximum population (carrying capacity)
+        P0=input$P0,       #popstat(YEAR=1999)
+        r =input$r,        #Population growth rate (logistic growth curve)
+        S0=0.0305853,
+        standard_start = 2004,
+        new_start = 2015,
+        
+        
+        #cost = treated1+treated2
+        total_HCC=0,
+        total_HCV=631000,
+        
+        FI= input$Fi,     #Influx rate of the population to become susceptible per year
+        
+        #Progression of fibrosis
+        f0f1=0.117,       #Fibrosis stage F0 to F1
+        f1f2=0.085,       #Fibrosis stage F1 to F2
+        f2f3=0.12,        #Fibrosis stage F2 to F3
+        f3c1=0.116,       #Fibrosis stage F3 to C1
+        
+        #Progression of cirrhosis
+        c1c2=0.044,       #Fibrosis stage C1 to C2
+        c2c3=0.044,       #Fibrosis stage C2 to C3
+        c3c4=0.076,       #Fibrosis stage C3 to C4
+        
+        #Incidence of developing HCC
+        c1bA=0.0068,      #Fibrosis stage C1 to bA
+        c1bB=0.0099,      #Fibrosis stage C1 to bB
+        c1bC=0.0029,      #Fibrosis stage C1 to bC
+        c1bD=0.0068,      #Fibrosis stage C1 to bD
+        
+        c2bA=0.0068,      #Fibrosis stage C2 to bA
+        c2bB=0.0099,      #Fibrosis stage C2 to bB
+        c2bC=0.0029,      #Fibrosis stage C2 to bC
+        c2bD=0.0068,      #Fibrosis stage C2 to bD
+        
+        c3bD=0.0664,      #Fibrosis stage C3 to bD
+        c4bD=0.0664,      #Fibrosis stage C4 to bD
+        
+        #Death rate from cirrhosis and HCC
+        deathc1=0.01,         #Death rate for Cirrhosis Child-Pugh class A
+        deathc2=0.01,         #Death rate for Cirrhosis Child-Pugh class B
+        deathc3=0.2,          #Death rate for Cirrhosis Child-Pugh class C
+        deathc4=0.57,         #Death rate for Cirrhosis Child-Pugh class D
+        
+        deathbA=1/(36/12),    #Death rate for HCC_BCLC_A
+        deathbB=1/(16/12),    #Death rate for HCC_BCLC_B
+        deathbC=1/(6/12),     #Death rate for HCC_BCLC_C
+        deathbD=1/(3/12),     #Death rate for HCC_BCLC_D
+        
+        deathtrn=1/(240/12),
+        
+        #Transplantation
+        tranc4=0.0015, #Transplantation rate in cirrhosis stage C4
+        tranbA=0.0015, #Transplantation rate in HCC_BCLC_A
+        tranbB=0.0015, #Transplantation rate in HCC_BCLC_B
+        #;newcase=17000,
+        cover = 5,
+        #Natural rate of death
+        natdeath=0.0424, #Unrelated to cirrhosis and hepatitis C
+        
+        beta= 0.327,              #Transmission coefficient
+        #Treatment efficacy
+        #Standard treatment response based on genotype (weighted average)
+        std_cureF0=0.7,
+        std_cureF1 =0.7,
+        std_cureF2 =0.7,
+        std_cureF3 =0.7,
+        std_cureC1 =0.7,
+        std_cureC2=0.7,
+        #Novel treatment response based on genotype (weighted average)
+        new_cureF0=0.985,
+        new_cureF1=0.985,
+        new_cureF2 =0.985,
+        new_cureF3 =0.985,
+        new_cureC1 =0.985,
+        new_cureC2 =0.985,
+        new_cureC3 =0.985,
+        new_cureC4 =0.985,
+        
+        std_dist = c(0.05,0.05,0.3,0.3,0.3)
+        
+      )
+    }
     
+    f_init <- function(S){
+      inits <- c(
+        S = S, #;0.01*#pop_since1960(TIME=1)
+        F0=0.2825*S,#;genotype1
+        F1=0.2825*S,
+        F2=0.184*S,
+        F3=0.124*S,
+        C1=0.03175*S,# ;CirA
+        C2=0.03175*S,#; CirA
+        C3=0.03175*S,#; CirB
+        C4=0.03175*S,#;CirC
+        
+        HCC_A=0,
+        HCC_B=0,
+        HCC_C=0,
+        HCC_D=0,
+        
+        C1std_cured=0,
+        C1new_cured=0,
+        C2new_cured=0,
+        C3new_cured=0,
+        C4new_cured=0,
+        
+        death = 0,
+        deathHCC = 0,
+        deathC14 = 0
+        
+      )
+    }
+    
+    FS <-  function(S0,P0){
+      S <- S0 * P0;
+    }
 
-    parms <- list(
-      K= 66785001,
-      P0=61623140, #popstat(YEAR=1999)
-      r =0.16,
-      S0=0.0305853,
-      standard_start = 2004,
-      new_start = 2015,
-      
-      
-      #cost = treated1+treated2
-      total_HCC=0,
-      total_HCV=631000,
-      
-      FI= 10^-8,
-      
-      f0f1=0.117,
-      f1f2=0.085,
-      f2f3=0.12,
-      f3c1=0.116,
-      c1c2=0.044,
-      c2c3=0.044,
-      c3c4=0.076,
-      c1bA=0.0068,
-      c1bB=0.0099,
-      c1bC=0.0029,
-      c1bD=0.0068,
-      
-      c2bA=0.0068,
-      c2bB=0.0099,
-      c2bC=0.0029,
-      c2bD=0.0068,
-      
-      c3bD=0.0664,
-      c4bD=0.0664,
-      deathc1=0.01,
-      deathc2=0.01,
-      deathc3=0.2,
-      deathc4=0.57,
-      deathbA=1/(36/12),
-      deathbB=1/(16/12),
-      deathbC=1/(6/12),
-      deathbD=1/(3/12),
-      deathtrn=1/(240/12),
-      tranc4=0.0015,
-      tranbA=0.0015,
-      tranbB=0.0015,
-      #;newcase=17000,
-      cover = 5,
-      natdeath=0.0424,
-      
-      beta=0.327,
-      std_cureF0=0.7,
-      std_cureF1 =0.7,
-      std_cureF2 =0.7,
-      std_cureF3 =0.7,
-      std_cureC1 =0.7,
-      std_cureC2=0.7,
-      new_cureF0=0.985,
-      new_cureF1=0.985,
-      new_cureF2 =0.985,
-      new_cureF3 =0.985,
-      new_cureC1 =0.985,
-      new_cureC2 =0.985,
-      new_cureC3 =0.985,
-      new_cureC4 =0.985,
-      std_dist = c(0.05,0.05,0.3,0.3,0.3)
-      
-    )
     
-    S <- parms$S0* parms$P0;
-    inits <- c(
-      S = S, #;0.01*#pop_since1960(TIME=1)
-      F0=0.2825*S,#;genotype1
-      F1=0.2825*S,
-      F2=0.184*S,
-      F3=0.124*S,
-      C1=0.03175*S,# ;CirA
-      C2=0.03175*S,#; CirA
-      C3=0.03175*S,#; CirB
-      C4=0.03175*S,#;CirC
+    f_data <- function(){
+      parms <- parameter()
       
-      HCC_A=0,
-      HCC_B=0,
-      HCC_C=0,
-      HCC_D=0,
+      S <- parms$S0* parms$P0;
       
-      C1std_cured=0,
-      C1new_cured=0,
-      C2new_cured=0,
-      C3new_cured=0,
-      C4new_cured=0,
+      #Initial status of model
+      inits <- f_init(S)
+        
       
-      death = 0,
-      deathHCC = 0,
-      deathC14 = 0
+      times <- seq(1999, 2020, by = 0.01)
       
-    )
-    times <- seq(1999, 2016, by = 0.01)
-    
-
-    
-    #out <-ode(yini3,times= times3,func = mysystem3,parms = pars3,method = "rk4")
-    #plot(out,select = c("S"))
-    out <- ode( y = inits,times =  times, func = PanHepC, parms = parms, method = "rk4")
-    
-#    output$distPlot <- renderPlot({
-#    plot(out,select = input$checkGroup,new=TRUE)
-#
-#    })
-    
-    out_df <- as.data.frame(out)
-    colnames(out_df)[23:28] <- c("prev","incHCC","pop","infect","total_HCV","total_HCC")
-
+      
+      
+      #out <-ode(yini3,times= times3,func = mysystem3,parms = pars3,method = "rk4")
+      #plot(out,select = c("S"))
+      out <- ode( y = inits,times =  times, func = PanHepC, parms = parms, method = "rk4")
+      
+      #    output$distPlot <- renderPlot({
+      #    plot(out,select = input$checkGroup,new=TRUE)
+      #     
+      #    })
+      
+      out_df <- as.data.frame(out)
+      colnames(out_df)[23:28] <- c("prev","incHCC","pop","infect","total_HCV","total_HCC")
+      out_df
+    }
+    isolate({
+    p <- parameter()
+    s <- FS(p$P0,p$S0)
+    i <- f_init(s)
+    data_p <- cbind(as.data.frame(p),as.data.frame(rbind(i)))
+    })
+    #output 1
     output$distPlot <- renderPlot({
       if (v$doPlot == FALSE) return()
-      
-      
-      isolate({
-      x <- out_df[,c(1,23)]
+      out_df <- f_data()
 
+      isolate({
+        p <- parameter()
+        s <- FS(p$P0,p$S0)
+        i <- f_init(s)
+        data_p <- cbind(as.data.frame(p),as.data.frame(rbind(i)))
+      x <- out_df[,c(1,23)]
       
+      
+      #time year >= input$year[1] , year <= input$year[2]
+      x_time <- out_df["time"] >= input$year[1] & out_df["time"] <= input$year[2]
+
+      x <- x[x_time,]
       x_melt <-melt(x, id="time")
       ggplot(data = x_melt) + 
         labs( x = "time", y = "Prevalence")+
         geom_line(mapping = aes(x = time, y = value,color = variable),size = 1.5)
-      
+        
       })
     })
     
-    
+    #output 2
     output$distPlot2 <- renderPlot({
       if (v$doPlot == FALSE) return()
-
-        x <- out_df[,c(1,21,22)]
+      out_df <- f_data()
+      x <- out_df[,c(1,21,22)]
+      
+      if(input$showgenotype){
         
-        if(input$showgenotype){
+        x <- out_df[,c(1,21,22,29)]
+      }
+      isolate({
 
-          x <- x %>% mutate(total_death = deathHCC+deathC14)
-        }
         
-        #time year >= 2005 , year <= 2010
-        x_time <- out_df["time"] >= 2005 & out_df["time"] <= 2010
+        #time year >= input$year[1] , year <= input$year[2]
+        x_time <- out_df["time"] >= input$year[1] & out_df["time"] <= input$year[2]
         
         x <- x[x_time,]
         x_melt <-melt(x, id="time")
@@ -187,24 +233,95 @@ shinyServer(function(input, output) {
         ggplot(data = x_melt) + 
           
           geom_line(mapping = aes(x = time, y = value,color = variable),size = 1.5)
-
+      })
     })
     
+    #output 3
     output$distPlot3 <- renderPlot({
       
       if (v$doPlot == FALSE) return()
-      
+      out_df <- f_data()
       x <- out_df[,c(1,27,28)]
       
       if(input$showgenotype2){
-        x <- out_df[,c(1,7:14)]
+        #x <- out_df[,c(1,7:14)]
+        x <- out_df[,c(1:21)]
       }
+      isolate({
+        
+        
+        #time year >= input$year[1] , year <= input$year[2]
+        x_time <- out_df["time"] >= input$year[1] & out_df["time"] <= input$year[2]
+        
+        x <- x[x_time,]
 
       x_melt <-melt(x, id="time")
       
       ggplot(data = x_melt) + 
         geom_line(mapping = aes(x = time, y = value,color = variable),size = 1.5)
-      
+      })
     })
+    
+    #output 4
+    output$distPlot4 <- renderPlot({
+      
+      if (v$doPlot == FALSE) return()
+      out_df <- f_data()
+      x <- out_df[,c(1,15:17)]
+      isolate({
+        
+        
+        #time year >= input$year[1] , year <= input$year[2]
+        x_time <- out_df["time"] >= input$year[1] & out_df["time"] <= input$year[2]
+        
+        x <- x[x_time,]
+      
+      x_melt <-melt(x, id="time")
+      
+      ggplot(data = x_melt) + 
+        geom_line(mapping = aes(x = time, y = value,color = variable),size = 1.5)
+      })
+    })
+    #textoutput
+    output$text1 <- renderText({
+      if(input$Treatment == 1){
+        "Treatment efficacy : 0.7 \n
+         Treatment cost : 10 $ per week"
+      } else if (input$Treatment == 2){
+        "Treatment efficacy : 0.9 \n
+         Treatment cost : 100 $ per week"
+      } else if (input$Treatment == 3){
+        "Treatment efficacy : 0.8 \n
+         Treatment cost : 50 $ per week"
+      } else if (input$Treatment == 4){
+        "Treatment efficacy : 0.9 \n
+         Treatment cost : 50 $ per week"
+      } else if (input$Treatment == 5){
+        "Treatment efficacy : 0.9 \n
+         Treatment cost : 50 $ per week"
+      }
+    })
+    
+    output$downloadData <- downloadHandler(
+      filename = function() {
+        paste("prev", ".csv", sep = "")
+      },
+      content = function(file) {
+        write.csv(f_data(), file, row.names = FALSE)
+      },contentType = "text/csv"
+    )
+
+    output$downloadData2 <-       
+
+    downloadHandler(
+
+      filename = "result.xlsx",
+      content = function(file) {
+        
+        write.xlsx(data_p, file, sheetName="Sheet1")
+        
+      }, contentType = "text/xlsx"
+      
+    )
 
 })
--- web/ui.R
+++ web/ui.R
@@ -19,22 +19,38 @@ shinyUI(fluidPage(
   
   # Application title
   tags$div(class="header bg-primary",
-  tags$h1(class="text-center","Hep C Model",class = "font-family-sans-serif")
+  tags$h1(class="text-center","Hepatitis C Virus Modelling",class = "font-family-sans-serif")
             ),
   tabsetPanel(
       #tab1
       tabPanel("Introduction",
-                  tags$h2("Abstract"),
+                  tags$h2("Rationale"),
                   tags$p("Hepatitis C virus (HCV) infection is an important worldwide public health problem, and most
                           of the global HCV burden is in low- to middle-income countries.A mathematical model of CHC transmission
                           dynamics was constructed to examine the disease burden over the next 20 years using different
                           treatment strategies. We compared and evaluated the current treatment (PEGylated
                           interferon and ribavirin) with new treatments using novel direct-acting antiviral agents
                           among various treatment policies."),
-                  
-                  tags$h2("Introduction"),
                   tags$p("These advances have led to the potential of HCV treatment delivery to national public
                           health programs and decreased overall HCV-related morbidity and mortality [1]."),
+               
+                  tags$h2("Study Objectives"),
+                  tags$p("1) To assess impact of treating HCV patients (by measuring the incidence of CHC - related decompensated
+                         cirrhosis and hepatocellular carcinoma (HCC) and mortality) wih different regimens including "),
+                  tags$ol(type = "a",
+                    tags$li("sofosbuvir with peginterferon alfa type 2a or 2b and ribavirin (National List of Essential Medicines)"),
+                    tags$li("sofosbuvir with ledipasvir (National List of Essential Medicines)"),
+                    tags$li("sofosbuvir with daclatasvir (pan-genotypic treatments)"),
+                    tags$li("sofosbuvir with velpatasvir (pan-genotypic treatments)"),
+                    tags$li("sofosbuvir with ravidasvir (pan-genotypic treatments, on-going clinical trial)")
+                  ),
+               
+                  tags$p("2) To estimate the cost of treatment for each regimen and compare with the standard treatment"),
+               
+                  tags$h2("Mathematical Modelling Approach"),
+                  tags$p("We further develop the model from our previous study looking at the 
+                         treatment coverage and allocation strategies (Poovorawan",tags$a(class = "italic","et. al."), 
+                         "2016)."),
                 tags$div(
                 tags$img(src="image/disease progression model.png" 
                           ,alt="Study design of the transmission and disease progression model."),
@@ -67,7 +83,7 @@ shinyUI(fluidPage(
                                  sliderInput("K",
                                              "Maximum population",
                                              min = 10000000,
-                                             max = 100000000,
+                                             max = 200000000,
                                              step = 1000000,
                                              value = 70000000
                                  )
@@ -75,19 +91,10 @@ shinyUI(fluidPage(
                         tags$div(class = "col-sm-4",
                                  sliderInput("r",
                                              "Population growth rate",
-                                             min = 0,
+                                             min = 0.01,
                                              max = 0.5,
                                              step = 0.01,
                                              value = 0.16
-                                 )
-                        ),
-                        tags$div(class = "col-sm-4",
-                                 sliderInput("F0",
-                                             "persons at fibrosis stage F0",
-                                             min = 0.1,
-                                             max = 0.3,
-                                             step = 0.001,
-                                             value = 0.282
                                  )
                         ),
                         tags$div(class = "col-sm-4",
@@ -99,15 +106,16 @@ shinyUI(fluidPage(
                                              value = 0.32
                                  )
                         ),
-                        tags$div(class = "col-sm-4",
-                                 sliderInput("S",
-                                             "proportion of the susceptible population",
+                        tags$div(class = "col-sm-8",
+                                 sliderInput("Fi",
+                                             "Influx rate of the population to become susceptible per year",
                                              min = 0.001,
-                                             max = 0.05,
+                                             max = 1,
                                              step = 0.001,
-                                             value = 0.032
+                                             value = 0.001
                                  )
                         ),
+
                         tags$h3("Progression of fibrosis"),
                         tags$hr(),
                         tags$div(class = "col-sm-4",
@@ -145,6 +153,7 @@ shinyUI(fluidPage(
                                              step = 0.001,
                                              value = 0.116
                                  )
+                                 
                         ),
                         tags$h3("Progression of cirrhosis"),
                         tags$hr(),
@@ -263,61 +272,26 @@ shinyUI(fluidPage(
       #tab3
       tabPanel("Treatment",
                tags$div(class = "sliderDisplay col-sm-12",
-                    
-                    tags$h3("Treatment efficacy"),
-                    tags$hr(),
-                    
-                    tags$div(class = "col-sm-6",
-                    sliderInput("stdC_E",
-                                "Standard Treatment Efficacy",
-                                min = 0,
-                                max = 1,
-                                step = 0.05,
-                                value = 0.7
-                                )
-                    ),
-                    
-                    tags$div(class = "col-sm-6",
-                    sliderInput("newC_E",
-                                "Novel Treatment Efficacy",
-                                min = 0,
-                                max = 1,
-                                step = 0.05,
-                                value = 0.9
-                                )
-                    ),
-                    
-                    
-
-                    tags$h3("Costs of treatments"),
-                    tags$hr(),
-                    
-                    tags$div(class = "col-sm-6",
-                    sliderInput("stdC_cost",
-                                "Standard Treatment Costs",
-                                min = 1,
-                                max = 100,
-                                step = 1,
-                                value = 10
-                    )
-                    ),
-                    
-                    tags$div(class = "col-sm-6",
-                    sliderInput("newC_cost",
-                                "Novel Treatment Costs",
-                                min = 1,
-                                max = 100,
-                                step = 1,
-                                value = 10
-                    )
-                    )
+                        tags$h3("Treatment"),
+                        tags$hr(),
+                        tags$div(class = "col-sm-12",
+                        radioButtons("Treatment", "Novel Treatment type:",width = '100%',
+                                     c("sofosbuvir with peginterferon alfa type 2a or 2b and ribavirin (National List of Essential Medicines)" =1,
+                                       "sofosbuvir with ledipasvir (National List of Essential Medicines)" = 2,
+                                       "sofosbuvir with daclatasvir (pan-genotypic treatments)" = 3,
+                                       "Log-sofosbuvir with velpatasvir (pan-genotypic treatments)" = 4,
+                                       "sofosbuvir with ravidasvir (pan-genotypic treatments, on-going clinical trial)" = 5))
+                        ),
+                        tags$div(class = "col-sm-11",id ="TreatmentOutput",
+                        textOutput("text1")
+                        )
                   
                )
                ), 
       #tab4
       tabPanel("Display Seting",
                tags$div(class = "sliderDisplay col-sm-12",
-                        sliderInput("time",
+                        sliderInput("year",
                                     "Time (Year)",
                                     min = 1999, 
                                     max = 2020,
@@ -326,13 +300,16 @@ shinyUI(fluidPage(
                )
                ),
       #tab5
-      tabPanel("Output",
+      tabPanel("Model prediction",
 
-                  actionButton("go", "Plot",class = "button btn btn-primary"),
+                  actionButton("go", "Run model",class = "button btn btn-primary"),
+
                  tabsetPanel(
                     #output 1
                     tabPanel("Prevalence Of CHC",
                              checkboxInput("bygenotype", "By Genotype", FALSE),
+                             downloadButton("downloadData", "Download"),
+                             downloadButton("downloadData2", "Download2"),
                              plotOutput("distPlot")
                              ),
                     #output 2
@@ -346,7 +323,12 @@ shinyUI(fluidPage(
                     tabPanel("Annual Incidence",
                              checkboxInput("showgenotype2", "By Genotype", FALSE),
                              plotOutput("distPlot3")
-                            )
+                            ),
+                    #output 4
+                    tabPanel("Estimated cost",
+                             checkboxInput("showgenotype3", "By Genotype", FALSE),
+                             plotOutput("distPlot4")
+                    )
                   )
                )
     )

91f03f98d85c2d42045529f0a6a876953b744b31
tanaphum <tana_jumbo@hotmail.com>
2018-10-18 09:45:01 +0700

    fixed file p1_gebotype and add test file
    - fixed file p1_gebotype can run in R
    - add test file to test run
    - can't not test fucntion bacause of parameters

--- p1_genotype.cpp
+++ p1_genotype.cpp
@@ -9,8 +9,8 @@ double popfn(double time, double K, double P0, double r){
 }
 
 // [[Rcpp::export]]
-List PanHepC(double time, NumericVector state, List parms){
-
+List PanHepC(double time, List state, List parms){
+  //Change NumericVector state to List state for collect NumericVector in state
   //get the value of each parameter from parms
   double K = parms["K"];
   double P0 = parms["P0"];
@@ -63,50 +63,51 @@ List PanHepC(double time, NumericVector state, List parms){
   
   double beta = parms["beta"];
   
-  double std_cureF0 = parms["std_cureF0"];
-  double std_cureF1 = parms["std_cureF1"];
-  double std_cureF2 = parms["std_cureF2"];
-  double std_cureF3 = parms["std_cureF3"];
-  double std_cureC1 = parms["std_cureC1"];
+  NumericVector std_cureF0 = parms["std_cureF0"];
+  NumericVector std_cureF1 = parms["std_cureF1"];
+  NumericVector std_cureF2 = parms["std_cureF2"];
+  NumericVector std_cureF3 = parms["std_cureF3"];
+  NumericVector std_cureC1 = parms["std_cureC1"];
   //double std_cureC2 = parms["std_cureC2"];
   
-  double new_cureF0 = parms["new_cureF0"];
-  double new_cureF1 = parms["new_cureF1"];
-  double new_cureF2 = parms["new_cureF2"];
-  double new_cureF3 = parms["new_cureF3"];
-  double new_cureC1 = parms["new_cureC1"];
-  double new_cureC2 = parms["new_cureC2"];
-  double new_cureC3 = parms["new_cureC3"];
-  double new_cureC4 = parms["new_cureC4"];
+  NumericVector new_cureF0 = parms["new_cureF0"];
+  NumericVector new_cureF1 = parms["new_cureF1"];
+  NumericVector new_cureF2 = parms["new_cureF2"];
+  NumericVector new_cureF3 = parms["new_cureF3"];
+  NumericVector new_cureC1 = parms["new_cureC1"];
+  NumericVector new_cureC2 = parms["new_cureC2"];
+  NumericVector new_cureC3 = parms["new_cureC3"];
+  NumericVector new_cureC4 = parms["new_cureC4"];
   
   //get the state values  
-####These are all vectors with genotype 1 to 6
-  double S = state["S"];
-  double C1 = state["C1"]; 
-  double C2 = state["C2"];
-  double C3 = state["C3"];
-  double C4 = state["C4"];
-  double F0 = state["F0"];
-  double F1 = state["F1"];
-  double F2 = state["F2"];
-  double F3 = state["F3"];
+  //These are all vectors with genotype 1 to 6
+  NumericVector S = state["S"];
+  NumericVector C1 = state["C1"]; 
+  NumericVector C2 = state["C2"];
+  NumericVector C3 = state["C3"];
+  NumericVector C4 = state["C4"];
+  NumericVector F0 = state["F0"];
+  NumericVector F1 = state["F1"];
+  NumericVector F2 = state["F2"];
+  NumericVector F3 = state["F3"];
   
-  double HCC_A = state["HCC_A"];
-  double HCC_B = state["HCC_B"];
-  double HCC_C = state["HCC_C"];
-  double HCC_D = state["HCC_D"];
+  NumericVector HCC_A = state["HCC_A"];
+  NumericVector HCC_B = state["HCC_B"];
+  NumericVector HCC_C = state["HCC_C"];
+  NumericVector HCC_D = state["HCC_D"];
   
-  double C1std_cured = state["C1std_cured"];
-  double C1new_cured = state["C1new_cured"];
-  double C2new_cured = state["C2new_cured"];
-  double C3new_cured = state["C3new_cured"];
-  double C4new_cured = state["C4new_cured"];
+  NumericVector C1std_cured = state["C1std_cured"];
+  NumericVector C1new_cured = state["C1new_cured"];
+  NumericVector C2new_cured = state["C2new_cured"];
+  NumericVector C3new_cured = state["C3new_cured"];
+  NumericVector C4new_cured = state["C4new_cured"];
   
-  double death = state["death"];
-  double deathHCC = state["deathHCC"];
-  double deathC14 = state["deathC14"];
-####These are all vectors with genotype 1 to 6   
-  double pop;
+  NumericVector death = state["death"];
+  NumericVector deathHCC = state["deathHCC"];
+  NumericVector deathC14 = state["deathC14"];
+  
+  //These are all vectors with genotype 1 to 6   
+  NumericVector pop;
   pop = popfn(time, K, P0, r);
     
   double treated1;
@@ -126,7 +127,8 @@ List PanHepC(double time, NumericVector state, List parms){
  
   NumericVector new_dist(8);  
   //F0
-###if(sum(C4) > 0)
+  //###
+  if(sum(C4) > 0)
     new_dist[0] = 0.05;
   else if(sum(C3) > 0 )
     new_dist[0] = 0.05;
@@ -146,7 +148,7 @@ List PanHepC(double time, NumericVector state, List parms){
     new_dist[0] = 0;
   
   //F1
-  if(C4 > 0)
+  if(sum(C4)> 0)
     new_dist[1] = 0.05;
   else if(sum(C3) > 0)
     new_dist[1] = 0.05;
@@ -231,89 +233,90 @@ List PanHepC(double time, NumericVector state, List parms){
     new_dist[7] = 0;
 
   NumericVector new_tr = new_dist*treated2;
-###
+//###
   
   double flowin = FI;
-  double infect = (F0+F1+F2+F3+C1+C2+C3+C4+HCC_A+HCC_B+HCC_C+HCC_D);
-###sum of "infect" from all 6 genotypes
-  double lam0 = beta*infect/pop;
-### S and all compartments are array of 6 genotypes
-  double dS = flowin*pop-lam0*S-natdeath*S;
+  NumericVector infect = (F0+F1+F2+F3+C1+C2+C3+C4+HCC_A+HCC_B+HCC_C+HCC_D);
+//###sum of "infect" from all 6 genotypes
+  NumericVector lam0 = beta*infect/pop;
+//### S and all compartments are array of 6 genotypes
+  NumericVector dS = flowin*pop-lam0*S-natdeath*S;
     
-  double dF0 = -f0f1*F0+lam0*S-(std_tr[0]*std_cureF0)-(new_tr[0]*new_cureF0)-natdeath*F0;
+  NumericVector dF0 = -f0f1*F0+lam0*S-(std_tr[0]*std_cureF0)-(new_tr[0]*new_cureF0)-natdeath*F0;
     
-  double dF1 =  f0f1*F0 -f1f2*F1-std_tr[1]*std_cureF1-new_tr[1]*new_cureF1-natdeath*F1;
+  NumericVector dF1 =  f0f1*F0 -f1f2*F1-std_tr[1]*std_cureF1-new_tr[1]*new_cureF1-natdeath*F1;
     
-  double dF2 = f1f2*F1-f2f3*F2-std_tr[2]*std_cureF2-new_tr[2]*new_cureF2-natdeath*F2;
+  NumericVector dF2 = f1f2*F1-f2f3*F2-std_tr[2]*std_cureF2-new_tr[2]*new_cureF2-natdeath*F2;
     
-  double dF3 = f2f3*F2-f3c1*F3-std_tr[3]*std_cureF3-new_tr[3]*new_cureF3-natdeath*F3;
+  NumericVector dF3 = f2f3*F2-f3c1*F3-std_tr[3]*std_cureF3-new_tr[3]*new_cureF3-natdeath*F3;
     
-  double dC1 = f3c1*F3-deathc1*C1-c1c2*C1-std_tr[4]*std_cureC1-new_tr[4]*new_cureC1-(c1bA+c1bB+c1bC+c1bD)*C1-natdeath*C1;
+  NumericVector dC1 = f3c1*F3-deathc1*C1-c1c2*C1-std_tr[4]*std_cureC1-new_tr[4]*new_cureC1-(c1bA+c1bB+c1bC+c1bD)*C1-natdeath*C1;
     
-  double dC2 = c1c2*C1-deathc2*C2-c2c3*C2-new_tr[5]*new_cureC2-(c2bA+c2bB+c2bC+c2bD)*C2-natdeath*C2;
+  NumericVector dC2 = c1c2*C1-deathc2*C2-c2c3*C2-new_tr[5]*new_cureC2-(c2bA+c2bB+c2bC+c2bD)*C2-natdeath*C2;
     
-  double dC3 = c2c3*C2-deathc3*C3-c3c4*C3-new_tr[6]*new_cureC3-c3bD*C3-natdeath*C3;
+  NumericVector dC3 = c2c3*C2-deathc3*C3-c3c4*C3-new_tr[6]*new_cureC3-c3bD*C3-natdeath*C3;
     
     
-  double dC4 = c3c4*C3-new_tr[7]*new_cureC4-deathc4*C4-c4bD*C4-natdeath*C4;
+  NumericVector dC4 = c3c4*C3-new_tr[7]*new_cureC4-deathc4*C4-c4bD*C4-natdeath*C4;
     
   
-  double dHCC_A = c1bA*(C1+C1std_cured+C1new_cured)+c2bA*(C2+C2new_cured)-deathbA*HCC_A-tranbA*HCC_A-natdeath*HCC_A;
+  NumericVector dHCC_A = c1bA*(C1+C1std_cured+C1new_cured)+c2bA*(C2+C2new_cured)-deathbA*HCC_A-tranbA*HCC_A-natdeath*HCC_A;
     
-  double dHCC_B = c1bB*(C1+C1std_cured+C1new_cured)+c2bB*(C2+C2new_cured)-deathbB*HCC_B-tranbB*HCC_B-natdeath*HCC_B;
+  NumericVector dHCC_B = c1bB*(C1+C1std_cured+C1new_cured)+c2bB*(C2+C2new_cured)-deathbB*HCC_B-tranbB*HCC_B-natdeath*HCC_B;
     
-  double dHCC_C = c1bC*(C1+C1std_cured+C1new_cured)+c2bC*(C2+C2new_cured)-deathbC*HCC_C-natdeath*HCC_C;
+  NumericVector dHCC_C = c1bC*(C1+C1std_cured+C1new_cured)+c2bC*(C2+C2new_cured)-deathbC*HCC_C-natdeath*HCC_C;
     
-  double dHCC_D = c1bD*(C1+C1std_cured+C1new_cured)+c2bD*(C2+C2new_cured)-deathbD*HCC_D+c3bD*(C3+C3new_cured)+c4bD*(C4+C4new_cured)-natdeath*HCC_D;
+  NumericVector dHCC_D = c1bD*(C1+C1std_cured+C1new_cured)+c2bD*(C2+C2new_cured)-deathbD*HCC_D+c3bD*(C3+C3new_cured)+c4bD*(C4+C4new_cured)-natdeath*HCC_D;
     
-  double ddeath =  deathc1*C1+deathc2*C2+deathc3*C3+deathc4*C4+deathbA*HCC_A+deathbB*HCC_B+deathbC*HCC_C+deathbD*HCC_D;
+  NumericVector ddeath =  deathc1*C1+deathc2*C2+deathc3*C3+deathc4*C4+deathbA*HCC_A+deathbB*HCC_B+deathbC*HCC_C+deathbD*HCC_D;
 
     
-  double ddeathHCC = deathbA*HCC_A+deathbB*HCC_B+deathbC*HCC_C+deathbD*HCC_D;
+  NumericVector ddeathHCC = deathbA*HCC_A+deathbB*HCC_B+deathbC*HCC_C+deathbD*HCC_D;
     
-  double ddeathC14 = deathc1*C1+deathc2*C2+deathc3*C3+deathc4*C4;
+  NumericVector ddeathC14 = deathc1*C1+deathc2*C2+deathc3*C3+deathc4*C4;
   
   
-  double dC1std_cured = std_tr[4]*std_cureC1-natdeath*(C1std_cured)-(c1bA+c1bB+c1bC+c1bD)*C1std_cured;
+  NumericVector dC1std_cured = std_tr[4]*std_cureC1-natdeath*(C1std_cured)-(c1bA+c1bB+c1bC+c1bD)*C1std_cured;
     
-  double dC1new_cured = new_tr[4]*new_cureC1-natdeath*(C1new_cured)-(c1bA+c1bB+c1bC+c1bD)*C1new_cured;
+  NumericVector dC1new_cured = new_tr[4]*new_cureC1-natdeath*(C1new_cured)-(c1bA+c1bB+c1bC+c1bD)*C1new_cured;
     
-  double dC2new_cured = new_tr[5]*new_cureC2-natdeath*(C1new_cured)-(c2bA+c2bB+c2bC+c2bD)*C2new_cured;
+  NumericVector dC2new_cured = new_tr[5]*new_cureC2-natdeath*(C1new_cured)-(c2bA+c2bB+c2bC+c2bD)*C2new_cured;
     
-  double dC3new_cured = new_tr[6]*new_cureC3-natdeath*(C3new_cured)-c3bD*C3new_cured;
+  NumericVector dC3new_cured = new_tr[6]*new_cureC3-natdeath*(C3new_cured)-c3bD*C3new_cured;
     
-  double dC4new_cured = new_tr[7]*new_cureC4-natdeath*(C4new_cured)-c4bD*C4new_cured;
+  NumericVector dC4new_cured = new_tr[7]*new_cureC4-natdeath*(C4new_cured)-c4bD*C4new_cured;
     
-  double new_tranLiv=tranc4*((C4-new_tr[7])+new_tr[7]*(1-new_cureC4))+tranbA*HCC_A+tranbB*HCC_B;
+  NumericVector new_tranLiv=tranc4*((C4-new_tr[7])+new_tr[7]*(1-new_cureC4))+tranbA*HCC_A+tranbB*HCC_B;
     
-  double new_HCV_HCC=c1bA*C1+c2bA*C2+c1bB*C1+c2bB*C2+c1bC*C1+c2bC*C2+c1bD*C1+c2bD*C2;
+  NumericVector new_HCV_HCC=c1bA*C1+c2bA*C2+c1bB*C1+c2bB*C2+c1bC*C1+c2bC*C2+c1bD*C1+c2bD*C2;
   
   
-###still calculate for each genotype, must sum again to get the total
-  double new_death = deathc1*C1+deathc2*C2+deathc3*C3+deathc4*C4+deathbA*HCC_A+deathbB*HCC_B+deathbC*HCC_C+deathbD*HCC_D;
+  //###still calculate for each genotype, must sum again to get the total
+  NumericVector new_death = deathc1*C1+deathc2*C2+deathc3*C3+deathc4*C4+deathbA*HCC_A+deathbB*HCC_B+deathbC*HCC_C+deathbD*HCC_D;
   
-  double HCC = HCC_A+HCC_B+HCC_C+HCC_D;
+  NumericVector HCC = HCC_A+HCC_B+HCC_C+HCC_D;
   
-  double incHCC = c1bA*(C1+C1std_cured+C1new_cured)+c2bA*(C2+C2new_cured)+c1bB*(C1+C1std_cured+C1new_cured)+c2bB*(C2+C2new_cured)+c1bC*(C1+C1std_cured+C1new_cured)+c2bC*(C2+C2new_cured)+c1bD*(C1+C1std_cured+C1new_cured)+c2bD*(C2+C2new_cured)+c3bD*(C3+C3new_cured)+c4bD*(C4+C4new_cured);
+  NumericVector incHCC = c1bA*(C1+C1std_cured+C1new_cured)+c2bA*(C2+C2new_cured)+c1bB*(C1+C1std_cured+C1new_cured)+c2bB*(C2+C2new_cured)+c1bC*(C1+C1std_cured+C1new_cured)+c2bC*(C2+C2new_cured)+c1bD*(C1+C1std_cured+C1new_cured)+c2bD*(C2+C2new_cured)+c3bD*(C3+C3new_cured)+c4bD*(C4+C4new_cured);
   
   
-###still calculate for each genotype, must sum again to get the total
+//###still calculate for each genotype, must sum again to get the total
   
+  //use sum() to F0,F1,F2,F3
   NumericVector propF(4);  
-  propF[0] = F0/(F0+F1+F2+F3);
-  propF[1] = F1/(F0+F1+F2+F3);
-  propF[2] = F2/(F0+F1+F2+F3);
-  propF[3] = F3/(F0+F1+F2+F3);
+  propF[0] = sum(F0)/(sum(F0)+sum(F1)+sum(F2)+sum(F3));
+  propF[1] = sum(F1)/(sum(F0)+sum(F1)+sum(F2)+sum(F3));
+  propF[2] = sum(F2)/(sum(F0)+sum(F1)+sum(F2)+sum(F3));
+  propF[3] = sum(F3)/(sum(F0)+sum(F1)+sum(F2)+sum(F3));
 
-###still calculate for each genotype, must sum again to get the total
+//###still calculate for each genotype, must sum again to get the total
       
-  double prev = 100*(infect/pop);
+  NumericVector prev = 100*(infect/pop);
   
-  double total_HCC = HCC_A+HCC_B+HCC_C+HCC_D;
-  double total_HCV = F0+F1+F2+F3+C1+C2+C3+C4;
+  NumericVector total_HCC = HCC_A+HCC_B+HCC_C+HCC_D;
+  NumericVector total_HCV = F0+F1+F2+F3+C1+C2+C3+C4;
     
-
-  NumericVector compartments(21);
+  //change NumericVector to List
+  List compartments(21);
   // compartments["S"] = dS;
   // compartments["F0"] = dF0;
   // compartments["F1"] = dF1;
@@ -369,7 +372,13 @@ List PanHepC(double time, NumericVector state, List parms){
   
 }
 
+// [[Rcpp::export]]
+NumericVector f(NumericVector F0,NumericVector F1){
+  return F0 + F1;
+}
 
 /*** R
-
+a= c(1,2,3)
+b = c (2,3,4)
+f(a,b)
 */
